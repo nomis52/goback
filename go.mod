424891830629a8d3818b5ab21f57cb735d52d066
Simon Newton <nomis52@gmail.com>
2025-06-09 15:58:19 +0800

    Separate packages.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module goback
+module github.com/nomis52/goback
 
 go 1.23.2
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/nomis52/goback/config"
+	"github.com/nomis52/goback/ipmi"
+)
+
+type Args struct {
+	ConfigPath string
+}
 
 func main() {
-	ctrl := NewIPMIController("10.0.0.245", "ADMIN", "ADMIN")
+	if err := doMain(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func doMain() error {
+	args := parseArgs()
+	if args.ConfigPath == "" {
+		return fmt.Errorf("-c or --config flag is required")
+	}
+
+	cfg, err := config.LoadConfig(args.ConfigPath)
+	if err != nil {
+		return fmt.Errorf("Error loading config: %w", err)
+	}
+
+	ctrl := ipmi.NewIPMIController(cfg.IPMI.Host, cfg.IPMI.Username, cfg.IPMI.Password)
 	s, err := ctrl.Status()
 
+	if err != nil {
+		return err
+	}
 	fmt.Println(s)
-	fmt.Println(err)
+	return nil
+}
+
+func parseArgs() Args {
+	configPath := flag.String("config", "", "Path to config file")
+	configPathShort := flag.String("c", "", "Path to config file (shorthand)")
+	flag.Parse()
+
+	path := *configPath
+	if path == "" && *configPathShort != "" {
+		path = *configPathShort
+	}
+	return Args{ConfigPath: path}
 }
