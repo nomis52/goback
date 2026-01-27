f2d34025aafd55297768b1aca0a25f68cc671ad3
Simon Newton <nomis52@gmail.com>
2026-01-27 21:18:13 +0800

    Add server properties

---
+++ server/properties.go
@@ -0,0 +1,8 @@
+package server
+
+import (
+	"github.com/nomis52/goback/server/types"
+)
+
+// ServerProperties is an alias for types.ServerProperties for convenience.
+type ServerProperties = types.ServerProperties
